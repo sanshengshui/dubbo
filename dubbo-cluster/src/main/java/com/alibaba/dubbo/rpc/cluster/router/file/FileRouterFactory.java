43a80d34f34a616e7d3031b78cf3757935cab78f
sanshengshui <lovewsic@gmail.com>
2018-08-15 18:08:10 +0800

    FileRouterFactory

--- dubbo-cluster/src/main/java/com/alibaba/dubbo/rpc/cluster/router/file/FileRouterFactory.java
+++ dubbo-cluster/src/main/java/com/alibaba/dubbo/rpc/cluster/router/file/FileRouterFactory.java
@@ -30,7 +30,9 @@ import java.io.IOException;
 public class FileRouterFactory implements RouterFactory {
 
     public static final String NAME = "file";
-
+    /**
+     * RouterFactory$Adaptive对象
+     */
     private RouterFactory routerFactory;
 
     public void setRouterFactory(RouterFactory routerFactory) {
@@ -42,7 +44,9 @@ public class FileRouterFactory implements RouterFactory {
         try {
             // Transform File URL into Script Route URL, and Load
             // file:///d:/path/to/route.js?router=script ==> script:///d:/path/to/route.js?type=js&rule=<file-content>
+            // 获得router配置项，默认为script
             String protocol = url.getParameter(Constants.ROUTER_KEY, ScriptRouterFactory.NAME); // Replace original protocol (maybe 'file') with 'script'
+            // 使用文件后缀做为类型
             String type = null; // Use file suffix to config script type, e.g., js, groovy ...
             String path = url.getPath();
             if (path != null) {
@@ -51,11 +55,12 @@ public class FileRouterFactory implements RouterFactory {
                     type = path.substring(i + 1);
                 }
             }
+            // 读取规则内容
             String rule = IOUtils.read(new FileReader(new File(url.getAbsolutePath())));
-
+            // 创建路由规则 URL
             boolean runtime = url.getParameter(Constants.RUNTIME_KEY, false);
             URL script = url.setProtocol(protocol).addParameter(Constants.TYPE_KEY, type).addParameter(Constants.RUNTIME_KEY, runtime).addParameterAndEncoded(Constants.RULE_KEY, rule);
-
+            // 通过 Dubbo SPI Adaptive 机制，获得 Router 对象
             return routerFactory.getRouter(script);
         } catch (IOException e) {
             throw new IllegalStateException(e.getMessage(), e);
