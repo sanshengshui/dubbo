5fbe5e0c364ee99a5488a00c853e06d17b1db40c
sanshengshui <lovewsic@gmail.com>
2018-08-24 13:52:18 +0800

    网络传输接口

--- dubbo-remoting/dubbo-remoting-api/src/main/java/com/alibaba/dubbo/remoting/Transporter.java
+++ dubbo-remoting/dubbo-remoting-api/src/main/java/com/alibaba/dubbo/remoting/Transporter.java
@@ -33,14 +33,13 @@ import javax.sound.midi.Receiver;
  */
 @SPI("netty")
 public interface Transporter {
-
     /**
      * Bind a server.
-     *
+     * 绑定一个服务器
      * @param url     server url
-     * @param handler
-     * @return server
-     * @throws RemotingException
+     * @param handler 通道处理器
+     * @return server 服务器
+     * @throws RemotingException 当绑定发生异常时
      * @see com.alibaba.dubbo.remoting.Transporters#bind(URL, Receiver, ChannelHandler)
      */
     @Adaptive({Constants.SERVER_KEY, Constants.TRANSPORTER_KEY})
@@ -48,11 +47,11 @@ public interface Transporter {
 
     /**
      * Connect to a server.
-     *
-     * @param url     server url
-     * @param handler
-     * @return client
-     * @throws RemotingException
+     * 连接一个服务器，即创建一个客户端
+     * @param url     server url 服务器地址
+     * @param handler 通道处理器
+     * @return client 客户端
+     * @throws RemotingException 当连接发生异常时
      * @see com.alibaba.dubbo.remoting.Transporters#connect(URL, Receiver, ChannelListener)
      */
     @Adaptive({Constants.CLIENT_KEY, Constants.TRANSPORTER_KEY})
