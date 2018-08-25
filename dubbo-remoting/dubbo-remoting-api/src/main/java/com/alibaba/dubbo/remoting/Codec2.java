d435245cc551a03afcefc6103828a95ed03a317b
sanshengshui <lovewsic@gmail.com>
2018-08-25 22:45:01 +0800

    编解码器接口

--- dubbo-remoting/dubbo-remoting-api/src/main/java/com/alibaba/dubbo/remoting/Codec2.java
+++ dubbo-remoting/dubbo-remoting-api/src/main/java/com/alibaba/dubbo/remoting/Codec2.java
@@ -26,14 +26,31 @@ import java.io.IOException;
 @SPI
 public interface Codec2 {
 
+    /**
+     * 编码
+     * @param channel 通道
+     * @param buffer
+     * @param message 消息
+     * @throws IOException 当编码发生异常时
+     */
     @Adaptive({Constants.CODEC_KEY})
     void encode(Channel channel, ChannelBuffer buffer, Object message) throws IOException;
 
+    /**
+     * 解码
+     * @param channel 通道
+     * @param buffer
+     * @return 消息
+     * @throws IOException 当解码发生异常
+     */
     @Adaptive({Constants.CODEC_KEY})
     Object decode(Channel channel, ChannelBuffer buffer) throws IOException;
 
 
     enum DecodeResult {
+        /**
+         * 需要更多输入,忽略一些输入
+         */
         NEED_MORE_INPUT, SKIP_SOME_INPUT
     }
 
