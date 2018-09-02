a4c8f91fb1a6c156ac0296cadebefbbe75cb35f7
sanshengshui <lovewsic@gmail.com>
2018-09-02 15:33:01 +0800

    响应回调接口

--- dubbo-remoting/dubbo-remoting-api/src/main/java/com/alibaba/dubbo/remoting/exchange/ResponseCallback.java
+++ dubbo-remoting/dubbo-remoting-api/src/main/java/com/alibaba/dubbo/remoting/exchange/ResponseCallback.java
@@ -23,14 +23,14 @@ public interface ResponseCallback {
 
     /**
      * done.
-     *
+     * 处理执行完成
      * @param response
      */
     void done(Object response);
 
     /**
      * caught exception.
-     *
+     * 处理发生异常
      * @param exception
      */
     void caught(Throwable exception);
