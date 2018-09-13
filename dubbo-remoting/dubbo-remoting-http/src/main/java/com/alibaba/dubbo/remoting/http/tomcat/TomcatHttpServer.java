0e9ba6dcd52d14a639aca945d0e36ae37e59703c
sanshengshui <lovewsic@gmail.com>
2018-09-13 22:47:00 +0800

    基于Tomcat的HTTP服务器实现类

--- dubbo-remoting/dubbo-remoting-http/src/main/java/com/alibaba/dubbo/remoting/http/tomcat/TomcatHttpServer.java
+++ dubbo-remoting/dubbo-remoting-http/src/main/java/com/alibaba/dubbo/remoting/http/tomcat/TomcatHttpServer.java
@@ -34,39 +34,49 @@ import java.io.File;
 public class TomcatHttpServer extends AbstractHttpServer {
 
     private static final Logger logger = LoggerFactory.getLogger(TomcatHttpServer.class);
-
+    /**
+     * 内嵌的Tomcat对象
+     */
     private final Tomcat tomcat;
-
+    /**
+     * URL对象
+     */
     private final URL url;
 
     public TomcatHttpServer(URL url, final HttpHandler handler) {
         super(url, handler);
 
         this.url = url;
+        //注册HttpHandler到DispatcherServlet中
         DispatcherServlet.addHttpHandler(url.getPort(), handler);
+        //创建内嵌的Tomcat对象
         String baseDir = new File(System.getProperty("java.io.tmpdir")).getAbsolutePath();
         tomcat = new Tomcat();
         tomcat.setBaseDir(baseDir);
         tomcat.setPort(url.getPort());
+        //最大线程数
         tomcat.getConnector().setProperty(
                 "maxThreads", String.valueOf(url.getParameter(Constants.THREADS_KEY, Constants.DEFAULT_THREADS)));
 //        tomcat.getConnector().setProperty(
 //                "minSpareThreads", String.valueOf(url.getParameter(Constants.THREADS_KEY, Constants.DEFAULT_THREADS)));
 
+        // 最大连接池
         tomcat.getConnector().setProperty(
                 "maxConnections", String.valueOf(url.getParameter(Constants.ACCEPTS_KEY, -1)));
-
+        //编码为UTF-8
         tomcat.getConnector().setProperty("URIEncoding", "UTF-8");
+        // 连接超时，60 秒
         tomcat.getConnector().setProperty("connectionTimeout", "60000");
 
         tomcat.getConnector().setProperty("maxKeepAliveRequests", "-1");
         tomcat.getConnector().setProtocol("org.apache.coyote.http11.Http11NioProtocol");
-
+        // 添加 DispatcherServlet 到 Tomcat 中
         Context context = tomcat.addContext("/", baseDir);
         Tomcat.addServlet(context, "dispatcher", new DispatcherServlet());
         context.addServletMapping("/*", "dispatcher");
+        // 添加 ServletContext 对象，到 ServletManager 中
         ServletManager.getInstance().addServletContext(url.getPort(), context.getServletContext());
-
+        //启动Tomcat
         try {
             tomcat.start();
         } catch (LifecycleException e) {
@@ -76,10 +86,11 @@ public class TomcatHttpServer extends AbstractHttpServer {
 
     @Override
     public void close() {
+        //标记关闭
         super.close();
-
+        // 移除 ServletContext 对象
         ServletManager.getInstance().removeServletContext(url.getPort());
-
+        // 关闭 Tomcat
         try {
             tomcat.stop();
         } catch (Exception e) {
