91dd1363df6a00919a1a28eb4d1604d0fd2a4f75
sanshengshui <lovewsic@gmail.com>
2018-09-13 22:18:38 +0800

    Servlet管理器,负责管理ServletContext

--- dubbo-remoting/dubbo-remoting-http/src/main/java/com/alibaba/dubbo/remoting/http/servlet/ServletManager.java
+++ dubbo-remoting/dubbo-remoting-http/src/main/java/com/alibaba/dubbo/remoting/http/servlet/ServletManager.java
@@ -25,25 +25,47 @@ import java.util.concurrent.ConcurrentHashMap;
  *
  */
 public class ServletManager {
-
+    /**
+     * 外部服务器端口,用于'servlet'的服务器端口
+     */
     public static final int EXTERNAL_SERVER_PORT = -1234;
-
+    /**
+     * 单例
+     */
     private static final ServletManager instance = new ServletManager();
 
+    /**
+     * ServletContext集合
+     */
     private final Map<Integer, ServletContext> contextMap = new ConcurrentHashMap<Integer, ServletContext>();
+
 
     public static ServletManager getInstance() {
         return instance;
     }
 
+    /**
+     * 添加ServletContext对象
+     * @param port 服务器端口
+     * @param servletContext ServletContext对象
+     */
     public void addServletContext(int port, ServletContext servletContext) {
         contextMap.put(port, servletContext);
     }
 
+    /**
+     * 移除ServletContext对象
+     * @param port 服务器端口
+     */
     public void removeServletContext(int port) {
         contextMap.remove(port);
     }
 
+    /**
+     * 获得ServletContext对象
+     * @param port 服务器端口
+     * @return ServletContext对象
+     */
     public ServletContext getServletContext(int port) {
         return contextMap.get(port);
     }
