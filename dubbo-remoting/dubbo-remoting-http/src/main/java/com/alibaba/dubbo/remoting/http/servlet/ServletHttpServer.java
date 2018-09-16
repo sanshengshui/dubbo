4fa9c92ab773be92353744f817efa6799269fd57
sanshengshui <lovewsic@gmail.com>
2018-09-16 17:43:04 +0800

    基于Servlet的服务器实现类

--- dubbo-remoting/dubbo-remoting-http/src/main/java/com/alibaba/dubbo/remoting/http/servlet/ServletHttpServer.java
+++ dubbo-remoting/dubbo-remoting-http/src/main/java/com/alibaba/dubbo/remoting/http/servlet/ServletHttpServer.java
@@ -25,6 +25,7 @@ public class ServletHttpServer extends AbstractHttpServer {
 
     public ServletHttpServer(URL url, HttpHandler handler) {
         super(url, handler);
+        //注册HttpHandler到DispatcherServlet中
         DispatcherServlet.addHttpHandler(url.getParameter(Constants.BIND_PORT_KEY, 8080), handler);
     }
 
