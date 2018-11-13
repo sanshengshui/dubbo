2973271aea4997e3d38052f67d28e5f813aec9c2
sanshengshui <lovewsic@gmail.com>
2018-11-13 11:24:19 +0800

    Exporter 监听器。

--- dubbo-rpc/dubbo-rpc-api/src/main/java/com/alibaba/dubbo/rpc/ExporterListener.java
+++ dubbo-rpc/dubbo-rpc-api/src/main/java/com/alibaba/dubbo/rpc/ExporterListener.java
@@ -26,7 +26,7 @@ public interface ExporterListener {
 
     /**
      * The exporter exported.
-     *
+     * 当服务暴露完成
      * @param exporter
      * @throws RpcException
      * @see com.alibaba.dubbo.rpc.Protocol#export(Invoker)
@@ -35,7 +35,7 @@ public interface ExporterListener {
 
     /**
      * The exporter unexported.
-     *
+     * 当服务取消暴露完成
      * @param exporter
      * @throws RpcException
      * @see com.alibaba.dubbo.rpc.Exporter#unexport()
