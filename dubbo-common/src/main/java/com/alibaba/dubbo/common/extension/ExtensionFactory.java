7b9b85cd3a1bfa60921feb1b29f26e03db4e728d
sanshengshui <lovewsic@gmail.com>
2018-11-16 13:18:42 +0800

    拓展工厂接口

--- dubbo-common/src/main/java/com/alibaba/dubbo/common/extension/ExtensionFactory.java
+++ dubbo-common/src/main/java/com/alibaba/dubbo/common/extension/ExtensionFactory.java
@@ -25,9 +25,11 @@ public interface ExtensionFactory {
     /**
      * Get extension.
      *
-     * @param type object type.
-     * @param name object name.
-     * @return object instance.
+     * 获得拓展对象
+     *
+     * @param type object type.  拓展接口
+     * @param name object name.  拓展名
+     * @return object instance. 拓展对象
      */
     <T> T getExtension(Class<T> type, String name);
 
