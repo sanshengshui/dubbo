c0b0b6d25b7be43d8019ab8c634a25ced05f9280
sanshengshui <lovewsic@gmail.com>
2018-11-16 13:17:11 +0800

    自动激活条件的标记

--- dubbo-common/src/main/java/com/alibaba/dubbo/common/extension/Activate.java
+++ dubbo-common/src/main/java/com/alibaba/dubbo/common/extension/Activate.java
@@ -50,6 +50,13 @@ public @interface Activate {
      * @return group names to match
      * @see ExtensionLoader#getActivateExtension(URL, String, String)
      */
+    /**
+     * Group过滤条件。
+     * <br />
+     * 包含{@link ExtensionLoader#getActivateExtension}的group参数给的值，则返回扩展。
+     * <br />
+     * 如没有Group设置，则不过滤。
+     */
     String[] group() default {};
 
     /**
@@ -63,12 +70,24 @@ public @interface Activate {
      * @see ExtensionLoader#getActivateExtension(URL, String)
      * @see ExtensionLoader#getActivateExtension(URL, String, String)
      */
+    /**
+     * Key过滤条件。包含{@link ExtensionLoader#getActivateExtension}的URL的参数Key中有，则返回扩展。
+     * <p/>
+     * 示例：<br/>
+     * 注解的值 <code>@Activate("cache,validatioin")</code>，
+     * 则{@link ExtensionLoader#getActivateExtension}的URL的参数有<code>cache</code>Key，或是<code>validatioin</code>则返回扩展。
+     * <br/>
+     * 如没有设置，则不过滤。
+     */
     String[] value() default {};
 
     /**
      * Relative ordering info, optional
      *
      * @return extension list which should be put before the current one
+     */
+    /**
+     * 排序信息，可以不提供。
      */
     String[] before() default {};
 
@@ -77,12 +96,18 @@ public @interface Activate {
      *
      * @return extension list which should be put after the current one
      */
+    /**
+     * 排序信息，可以不提供。
+     */
     String[] after() default {};
 
     /**
      * Absolute ordering info, optional
      *
      * @return absolute ordering info
+     */
+    /**
+     * 排序信息，可以不提供。
      */
     int order() default 0;
 }
