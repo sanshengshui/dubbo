d6c202ff43747690f4115f30e23cf946da4cc2fe
sanshengshui <lovewsic@gmail.com>
2018-11-19 17:27:03 +0800

    实现 ExporterListener 接口，ExporterListener 适配器抽象类。

--- dubbo-rpc/dubbo-rpc-api/src/main/java/com/alibaba/dubbo/rpc/listener/ExporterListenerAdapter.java
+++ dubbo-rpc/dubbo-rpc-api/src/main/java/com/alibaba/dubbo/rpc/listener/ExporterListenerAdapter.java
@@ -22,6 +22,7 @@ import com.alibaba.dubbo.rpc.RpcException;
 
 /**
  * ExporterListenerAdapter
+ * 实现 ExporterListener 接口，ExporterListener 适配器抽象类。
  */
 public abstract class ExporterListenerAdapter implements ExporterListener {
 
