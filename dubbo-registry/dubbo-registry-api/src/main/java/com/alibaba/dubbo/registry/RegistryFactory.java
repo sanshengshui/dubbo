d7a5c6222aa3ca2fc4d4ca4df9b60a74db26d7da
sanshengshui <lovewsic@gmail.com>
2018-11-26 13:15:34 +0800

    注册中心工厂接口

--- dubbo-registry/dubbo-registry-api/src/main/java/com/alibaba/dubbo/registry/RegistryFactory.java
+++ dubbo-registry/dubbo-registry-api/src/main/java/com/alibaba/dubbo/registry/RegistryFactory.java
@@ -29,9 +29,15 @@ import com.alibaba.dubbo.common.extension.SPI;
 public interface RegistryFactory {
 
     /**
-     * Connect to the registry
+     * Connect to the registry 连接注册中心
      * <p>
-     * Connecting the registry needs to support the contract: <br>
+     * Connecting the registry needs to support the contract: <br> 连接注册中心需处理契约
+     * 1. 当设置check=false时表示不检查连接，否则在连接不上时抛出异常。
+     * 2. 支持URL上的username:password权限认证。
+     * 3. 支持backup=10.20.153.10备选注册中心集群地址。
+     * 4. 支持file=registry.cache本地磁盘文件缓存。
+     * 5. 支持timeout=1000请求超时设置。
+     * 6. 支持session=60000会话超时或过期设置。
      * 1. When the check=false is set, the connection is not checked, otherwise the exception is thrown when disconnection <br>
      * 2. Support username:password authority authentication on URL.<br>
      * 3. Support the backup=10.20.153.10 candidate registry cluster address.<br>
@@ -39,8 +45,8 @@ public interface RegistryFactory {
      * 5. Support the timeout=1000 request timeout setting.<br>
      * 6. Support session=60000 session timeout or expiration settings.<br>
      *
-     * @param url Registry address, is not allowed to be empty
-     * @return Registry reference, never return empty value
+     * @param url Registry address, is not allowed to be empty 注册中心地址，不允许为空
+     * @return Registry reference, never return empty value 注册中心引用，总不返回空
      */
     @Adaptive({"protocol"})
     Registry getRegistry(URL url);
