feabc76b25b82ce988e92a36312244e1b5643a35
sanshengshui <lovewsic@gmail.com>
2018-11-26 13:29:13 +0800

    实现 Invoker 接口，服务提供者 Invoker Wrapper

--- dubbo-registry/dubbo-registry-api/src/main/java/com/alibaba/dubbo/registry/support/ProviderInvokerWrapper.java
+++ dubbo-registry/dubbo-registry-api/src/main/java/com/alibaba/dubbo/registry/support/ProviderInvokerWrapper.java
@@ -26,10 +26,25 @@ import com.alibaba.dubbo.rpc.RpcException;
  * @date 2017/11/23
  */
 public class ProviderInvokerWrapper<T> implements Invoker {
+    /**
+     * Invoker对象
+     */
     private Invoker<T> invoker;
+    /**
+     * 原始 URL
+     */
     private URL originUrl;
+    /**
+     * 注册中心 URL
+     */
     private URL registryUrl;
+    /**
+     * 服务提供者URL
+     */
     private URL providerUrl;
+    /**
+     * 是否注册
+     */
     private volatile boolean isReg;
 
     public ProviderInvokerWrapper(Invoker<T> invoker,URL registryUrl,URL providerUrl) {
